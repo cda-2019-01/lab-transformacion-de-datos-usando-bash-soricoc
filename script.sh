7711c9aeaecfa55d8a856c3fdea3bd23fdb40a9c
Sol Beatriz Rico C <soricoc@unal.edu.co>
2019-06-04 20:01:16 -0500

    El resultado esperado y el generado por el script son comparados en Diff Checker y son iguales, pero el programa que califica sigue reportando que hay error.

--- script.sh
+++ script.sh
@@ -1,21 +1,28 @@
 for file in $(find . -name '*.csv');
+for file in $(find . -name '*.csv');                                      # Recorre los archivos csv de la carpeta	
 do
-    fn="$(basename -- ${file})";
+    fn="$(basename -- ${file})";                                          # Extrae el nombre del archivo 
     i=0
-    while read line || [ -n "$line" ];
+    while read line || [ -n "$line" ];                                    # Lee el archivo línea por línea
     do
-        if [ ${#line} -gt 1 ]
+        if [ ${#line} -gt 1 ]                                             # Salta las líneas en blanco
         then
-            ((i++))
-            cadenas=$(echo $line | tr "," "\n");
-            upp=$(echo $line | head -c 1);
-            for cad in $cadenas
+            ((i++))                                                       # Inicia un contador 
+            cadenas=$(echo $line | tr "," "\n");                          # Separa lo que se encuentra separado por coma
+            upp=$(echo $line | head -c 1);                                # Guarda en una variable la letra en mayúscula al inicio de cada línea
+            for cad in $cadenas                                           # Recorre las cadenas que se separaron por coma en un paso anterior
             do
-                if [[ "$cad" =~ ...:. ]]
+                if [[ "$cad" =~ ...:. ]]                                  # Valida si la cadena cumple con el patrón ...:.
                 then
-                    echo "$fn,$i,$upp,"$cad"";
+                    if [[ ${cad: -1} = $'\r' ]];                          # Valida retorno de carro
+                    then
+                        echo "$fn,$i,$upp,"$cad"";                        # Imprime la línea si retorno porque ya lo incluye
+                    else
+                        echo "$fn,$i,$upp,"$cad"" | sed -e "s/$/\r/";     # Imprime la línea con retorno de carro
+                    fi
                 fi
             done
         fi
     done < $fn
 done
+
